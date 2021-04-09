b3c868c696a8d7871f1798e21245330cda5f5f0d
Skyler Harris <skyler.iqeq@gmail.com>
2021-04-09 15:21:35 +0800

    search filter attempt (unfinished)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 baguetteBox.run('.photo_gallery');
-
 const search = new Filter('search', 'data-caption');
+
+function searchFilter() {
+   var input, filter, ul, li, a, i, txtValue;
+   input = document.getElementById('myinput');
+   filter = input.value.to.UpperCase();
+   ul = documnet.getElementById("myUL");
+   li = ul.getElementsByTagName('li');
+}
