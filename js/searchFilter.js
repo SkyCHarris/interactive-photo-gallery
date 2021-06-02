e6c716c00664d79b3ec0b2776f6d1f44d648035f
Skyler Harris <skyler.iqeq@gmail.com>
2021-06-02 16:52:54 +0800

    move for loop inside function, remove old search filter, switch getElementsByClass to getElementsByTagName

--- js/searchFilter.js
+++ js/searchFilter.js
@@ -24,14 +24,16 @@
 function search_photo() {
     let input = document.getElementById('search').value
     input=input.toLowerCase();
-    let x = document.getElementsByClassName('a');
+    let x = document.getElementsByTagName('a');
+    
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+           x[i].style.display="none";
+       }
+       else {
+           x[i].style.display="list-item";
+       }
+    }
  }
  
- for (i = 0; i < x.length; i++) {
-     if (!x[i].innerHTML.toLowerCase().includes(input)) {
-        x[i].style.display="none";
-    }
-    else {
-        x[i].style.display="list-item";
-    }
- }
+ 
